2190ea1129d078d7ada15dbfac20fb0cb37cfc9b
Joachim Vandekerckhove <joachimvandekerckhove@users.noreply.github.com>
2024-02-19 12:34:12 -0800

    Revisions for journal

--- code/figure3.R
+++ code/figure3.R
@@ -25,7 +25,7 @@ HDIofMCMC_quantile <- function(sampleVec,credMass=0.95){
 # Parameters of simulation
 successes <- 9
 failures <- 1
-rope <- c(0.4,0.6)
+rope <- c(0.388,0.612)
 set.seed(123)
 post_samples <- rbeta(9000,1+successes,1+failures)
 
@@ -69,11 +69,11 @@ centered_polygon <- function(center=c(0,0),width=1,height=1,...){
 
 
 # Figure begins
-postscript(file='transformation.eps',width=3,height=4)
+pdf(file='figure3.pdf',width=3.5,height=4)
 #pdf(file='transformation.pdf',width=3,height=4)
 par(mai=rep(0,4),cex.axis=0.7,tck=-0.02)
 k <- 5 # ylim in both directions
-plot(NULL,xlim=c(-0.05,1.05),ylim=c(-k,k))
+plot(NULL,xlim=c(-0.05,1.05),ylim=c(-k,k),axes=F,ann=F)
 	# 1) ROPE polygon
 	rope_st <- scaled_transformation(rope)
 	polygon(x=c(rope_st,rope_st[2],rope[2],rope[2],
@@ -88,33 +88,32 @@ plot(NULL,xlim=c(-0.05,1.05),ylim=c(-k,k))
 	# 3a) White boxes around the axes labels
 	at <- seq(0,1,0.25)
 	for(a in 1:length(at)){
-		centered_polygon(c(at[a],.575),.1,.4,col='#ffffff',border=F)
-		centered_polygon(c(at[a],-.575),.1,.4,col='#ffffff',border=F)
+		centered_polygon(c(at[a],.57),.075,.35,col='#ffffffdd',border=F)
+		centered_polygon(c(at[a],-.57),.065,.35,col='#ffffffdd',border=F)
 	}
-	# 3b) Axes and axes labels
-	axis(1,pos=1,padj=-2,at=seq(0,1,0.25))
-	axis(1,pos=-1,padj=-6,tck=0.02,
-	     at=seq(0,1,0.25),
-	     labels=round(transformation(seq(0,1,0.25)),2))
 	# 4) Panel labels
 	text(.2,2,expression(theta))
 	text(.2,-2,expression(paste('logit(',theta,')')))
 	# 5) Density curves
-	ht_original <- hist(post_samples,breaks=seq(0,1,length.out=200),plot=F)
+	ht_original <- hist(post_samples,breaks=seq(0,1,length.out=150),plot=F)
 	tr_samples <- transformed_samples[transformed_samples>=0&transformed_samples<=1]
-	ht_transformed <- hist(tr_samples,breaks=seq(0,1,length.out=200),plot=F)
-	lines(ht_original$mids,scaled_density(ht_original$density))
-	lines(ht_transformed$mids,-scaled_density(ht_transformed$density))
+	ht_transformed <- hist(tr_samples,breaks=seq(0,1,length.out=150),plot=F)
+	lines(ht_original$mids,scaled_density(ht_original$density),col='#555555')
+	lines(ht_transformed$mids,-scaled_density(ht_transformed$density),col='#555555')
 	# 5) HDIs
-	segments(x0=HDIofMCMC_quantile(post_samples),y0=rep(1,length(post_samples)),
-	 	x1=HDIofMCMC_quantile(transformed_samples),y1=rep(-1,length(post_samples)),
-		col='#000000',lwd=2)
-	segments(x0=HDIofMCMC_flawed(post_samples),y0=rep(1,length(post_samples)),
-	 	x1=HDIofMCMC_flawed(transformed_samples),y1=rep(-1,length(post_samples)),
-		col='#000000',lwd=2,lty='21')
+lines(c(HDIofMCMC_quantile(post_samples)[1],HDIofMCMC_quantile(transformed_samples)[1]),c(1,-1),lwd=1.75)
+lines(c(HDIofMCMC_quantile(post_samples)[2],HDIofMCMC_quantile(transformed_samples)[2]),c(1,-1),lwd=1.75)
+lines(c(HDIofMCMC_flawed(post_samples)[1],HDIofMCMC_flawed(transformed_samples)[1]),c(1,-1),lwd=1.75,lty='21')
+lines(c(HDIofMCMC_flawed(post_samples)[2],HDIofMCMC_flawed(transformed_samples)[2]),c(1,-1),lwd=1.75,lty='21')
+	# 3b) Axes and axes labels
+	axis(1,pos=1,padj=-2,at=seq(0,1,0.25),
+		labels=paste(seq(0,1,0.25)))
+	axis(1,pos=-1,padj=-6,tck=0.02,
+	     at=seq(0,1,0.25),
+	     labels=round(transformation(seq(0,1,0.25)),2))
 	# 6) Equally-spaced points
-	points(equally_spaced,rep(1,length(equally_spaced)),pch=21,cex=0.65,bg='#ffffff',col='#bbbbbb')
-	points(scaled_transformation(equally_spaced),rep(-1,length(equally_spaced)),pch=21,cex=0.65,bg='#ffffff',col='#bbbbbb')
+	points(equally_spaced,rep(1,length(equally_spaced)),pch=21,cex=0.65,bg='#ffffff',col='#dddddd')
+	points(scaled_transformation(equally_spaced),rep(-1,length(equally_spaced)),pch=21,cex=0.65,bg='#ffffff',col='#dddddd')
 	# 7) Legend
 	legend(0,-4.5,yjust=0.5,xjust=0,lwd=2,cex=.65,
 	       seg.len=1.5,x.intersp=0.5,pt.cex=1.25,
